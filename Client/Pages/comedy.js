6452e12a2331670415268987e5285bc94e3c6a5b
Skarader <lukasholmwolf@gmail.com>
2024-02-16 20:56:04 +0100

    test för comedydel, mockup

--- Client/Pages/comedy.js
+++ Client/Pages/comedy.js
@@ -1,11 +1,31 @@
-export default function renderComedy() {
-  $("main").attr("id", "homepage");
+export default async function renderComedy() {
+  $("main").attr("id", "comedy");
+
+  const response = await fetch("/api/events");
+  const result = await response.json();
+
+  console.log(result);
+
+  let eventString = "";
+  for (let event of result) {
+    if (event.date) {
+      const dateString = event.date.substring(0, 10);
+
+      eventString += `<p id="hello">${event.name}</p>
+    <p>${dateString}</p>
+    <p>Participant limit: ${event.participantLimit}</p>
+    <p>Avalible tickets: ${event.availableTickets}</p>
+    <br>
+    `;
+    }
+  }
 
   return `
-    
+        ${eventString}
         <main>
+            <div class="comedy-site-title-container">
             <h1 class="comedy-site-title">ClubFun</h1>
-
+            </div>
             <section class="comedy-about">
                 <h2 class="comedy-about-title">ClubFun Comedy</h2>
                 <p class="comedy-about-description">
@@ -21,7 +41,7 @@ export default function renderComedy() {
                 <div class="event-card">
                     <div class="event-card-date">15 Maj</div>
                     <div class="event-card-time">20:00 - 23:00</div>
-                    <div class="event-card-description">Tonight...</div>
+                    <div class="event-card-description"></div>
                     <button class="buy-ticket">Buy Ticket</button>
 
                 </div>
@@ -50,7 +70,7 @@ export default function renderComedy() {
             
 
         </main>
-
+        
 
     `;
 }
