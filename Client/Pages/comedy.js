540f5d18ab0adfcc913119ca5f0f42e8d892246b
Skarader <lukasholmwolf@gmail.com>
2024-02-18 02:07:13 +0100

    scrollbar for events, minor css changes

--- Client/Pages/comedy.js
+++ Client/Pages/comedy.js
@@ -5,10 +5,9 @@ export default async function renderComedy() {
   let eventCards = "";
   let dateString = "";
   let timeString = "";
-  let count = 0; // Räknare för antal event
 
   for (let event of result) {
-    if (event.date && count < 3) {
+    if (event.date) {
       dateString = event.date.substring(0, 10);
       timeString = event.date.substring(11, 16);
 
@@ -21,30 +20,27 @@ export default async function renderComedy() {
             <button class="buy-ticket">Buy Ticket</button>
           </div>
         `;
-
-      count++; // Öka räknaren för varje event som läggs till
     }
   }
 
   return `
-      <main class="test">
+      <main class="main">
         <div class="comedy-site-title-container">
           <h1 class="comedy-site-title">ClubFun</h1>
         </div>
         <section class="comedy-about">
           <h2 class="comedy-about-title">ClubFun Comedy</h2>
           <p class="comedy-about-description">
-            Welcome to the newest hottest comedy club in town.
-            With a mix of today's superstars and tomorrow's talents, our shows have something for everyone.
+          Välkommen till den senaste och hetaste komediklubben i stan. Med en blandning av dagens superstjärnor och morgondagens talanger erbjuder våra shower något för alla smaker.
+          Här, i denna vibrerande atmosfär av skratt och glädje, smälter dagens bekymmer bort och lämnar plats för en stund av avkoppling och förnyad energi.
           </p>
         </section>
   
         <div class="break-line"></div>
   
-        <h2 class="comedy-upcoming-events-title">Next Events</h2>
+        <h2 class="comedy-upcoming-events-title">Upcoming Events</h2>
         <section class="comedy-upcoming-events">
           ${eventCards}
-          <button class="all-events">Click here to view all upcoming events</button>
         </section>
       </main>
     `;
