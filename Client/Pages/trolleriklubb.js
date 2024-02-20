0c90d299ec2d58fb6b7d6dc8240f1d470e369bb3
EddieMollerstrom <eddiemollerstrom123@gmail.com>
2024-02-20 14:38:21 +0100

    added dialog

--- Client/Pages/trolleriklubb.js
+++ Client/Pages/trolleriklubb.js
@@ -1,29 +1,60 @@
 export default async function trolleriKlubb() {
     $("main").attr("id", "trolleriklubb")
 
-    const response = await fetch('/api/events')
+    const response = await fetch('/api/events/clubs/65d316622d34b531073334e3')
     const result = await response.json()
 
-    console.log(result)
+    
 
 
 
     let eventString = "";
     for (let event of result ){
+        const dateString = event.date;
+                // Konverterar datumet till ett datum objekt
+                const dateObject = new Date(dateString);
+                // Använder datum objektet för att få fram vilken månad det är (förkortat)
+                const month = dateObject.toLocaleDateString("sv-SE", { month: "short" }).toUpperCase().replace('.', '')
+                const day = dateObject.toLocaleDateString("sv-SE", { day: "numeric" })
+
         eventString += `
-        <div class="kommande-event">
-        <h3>${event.club.name}</h3>
-        <p>${event.name}</p>
+        <div class="kommande-event" data-event-id="${event._id}">
+        <h3>${event.name}</h3>
+        <p>${event.about}</p>
         <br>
         <p>Participant limit: ${event.participantLimit}</p>
         <br>
-        <p>${event.date}</p>
-        <button>Boka</button>
+        <p>${day}</p>
+        <p>${month}</p>
+        <button id="boka-btn">Boka</button>
         </div>
         `
     }
 
-    //<img id="trolleri-background" draggable="false" src="../Images/trolleriklubb.jpg">
+    $(document).on('click', '#boka-btn', async function () {
+        // Hämta det specifika eventets id från det närliggande DOM-elementet
+        const eventId = $(this).closest('.kommande-event').data('event-id');
+    
+        // Gör något baserat på eventets id
+        console.log('Klickade på knappen för event med id:', eventId);
+
+        try {
+            const response = await fetch(`/api/bookings/events/${eventId}`, { method: "POST"})
+            const result = await response.json();
+            $("#trolleri-dialog-h3").text(result.message)
+            document.querySelector("dialog").showModal()
+        }
+        catch(error) {
+            console.log(res.status(500).json({message: "Något gick fel"}))
+        }
+    });
+
+    $(document).on("click", "#trolleri-close-dialog", function (){
+        document.getElementById("trolleri-dialog").close()
+    })
+    
+
+
     return `
     <div id="logga-container">
         <h1 id="logga">SimSalaBim</h1>
@@ -38,11 +69,13 @@ export default async function trolleriKlubb() {
             <h2>Välkommna till <span id="orange-color">SimSalaBim</span>!</h2>
             <p>"Simsalabim" är en förtrollande klubb som bjuder in människor att upptäcka magins värld. Varje vecka anordnar klubben spännande och underhållande evenemang där besökare får uppleva en mix av illusioner, trolleri och magiska uppträdanden.
             Klubben, med sin unika atmosfär och charm, är en samlingsplats för både amatörer och professionella magiker samt för alla som älskar att bli förvånade och underhållna. Med sitt engagerade team av magiska talanger strävar "Simsalabim" efter att skapa minnesvärda upplevelser för alla sina gäster.
-            Miss inte chansen att delta i "Simsalabims" veckovisa evenemang och låt dig bli förtrollad av den magiska världen som klubben har att erbjuda!
+            Missa inte chansen att delta i "Simsalabims" veckovisa evenemang och låt dig bli förtrollad av den magiska världen som klubben har att erbjuda!
             </p>
     
         </div>
     </section>
+
+    
 
 
     <section class="kommande-event-container">
@@ -51,5 +84,13 @@ export default async function trolleriKlubb() {
             ${eventString}
         </div>
     </section>
+
+    <dialog id="trolleri-dialog">
+        <div id="trolleri-dialog-div">
+            <h3 id="trolleri-dialog-h3">test</h3>
+            <button id="trolleri-close-dialog">Stäng</button>
+        </div>
+        </dialog>
     `
 }
+
