4376500397b5553e8e4a542945b286211f1be61c
Valon02 <Valonahmeti95@outlook.com>
2024-02-21 15:44:08 +0100

    flyttat bilder och fixat så man kan boka events korrekt

--- Client/Pages/kodklubben.js
+++ Client/Pages/kodklubben.js
@@ -4,7 +4,7 @@ export default async function kodklubben() {
     const response = await fetch(`/api/events/clubs/65d326f62d34b531073334fb`);
     const result = await response.json();
     console.log(result);
-    let eventId = ""
+    let eventId = "";
 
     // Hämtar alla eventen och lägger till rätt styling för kommande evenemang
     let eventString = "";
@@ -13,55 +13,6 @@ export default async function kodklubben() {
         const dateObject = new Date(dateString);
         const month = dateObject.toLocaleDateString("sv-SE", { month: "short" }).toUpperCase().replace('.', '');
         const day = dateObject.toLocaleDateString("sv-SE", { day: "numeric" });
-
-         // Lägg till en klickhändelse för knapparna med klassen "event-button"
-         $(document).on('click', '.kod-event-button', async function () {
-
-            // Hämta det specifika eventets id från det närliggande DOM-elementet
-            eventId = $(this).closest('.kommande-event').data('event-id');
-            console.log('Klickade på knappen för event med id:', eventId);
-
-            try {
-                    // Open modal
-                    $('dialog').get(0).showModal()
-                    
-            } catch (error) {
-                    console.log(res.status(500).json({ message: "Något gick fel vid bokningen av eventet.", error: error }))
-            }
-    });
-
-
-    // Submit
-    $(document).on('click', '#kod-submit', async function (event) {
-            event.preventDefault()
-
-            // Input fält värden
-            var formData = {
-                    quantity: $('[name=quantity]').val()
-            };
-
-            // Fetch
-            const response = await fetch(`/api/bookings/events/${eventId}`,
-                    {
-                            method: "POST",
-                            headers: { "Content-Type": "application/json" },
-                            body: JSON.stringify(formData)
-                    }
-                    )
-
-            const bookingData = await response.json();
-            console.log(bookingData);
-            $("#meddelande").text(bookingData.message)
-
-            //console.log(formData.quantity);
-            //console.log(bookingData.message);    
-    })
-
-    // Close modal
-    $(document).on('click', '#close-dialog', function () {
-            $('dialog').get(0).close()
-    })
-
 
         eventString += `
             <div class="kod-kommande-event" data-event-id="${event._id}">
@@ -83,11 +34,51 @@ export default async function kodklubben() {
             </div>`;
     }
 
+    // Lägg till en klickhändelse för knapparna med klassen "event-button"
+    $(document).on('click', '.kod-event-button', async function () {
+        // Hämta det specifika eventets id från det närliggande DOM-elementet
+        eventId = $(this).closest('.kod-kommande-event').data('event-id');
+        console.log('Klickade på knappen för event med id:', eventId);
+
+        try {
+            // Open modal
+            $('dialog').get(0).showModal();
+        } catch (error) {
+            console.log(res.status(500).json({ message: "Något gick fel vid bokningen av eventet.", error: error }));
+        }
+    });
+
+    // Submit
+    $(document).on('click', '#kod-submit', async function (event) {
+        event.preventDefault();
+
+        // Input fält värden
+        var formData = {
+            quantity: $('[name=quantity]').val()
+        };
+
+        // Fetch
+        const response = await fetch(`/api/bookings/events/${eventId}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData)
+        });
+
+        const bookingData = await response.json();
+        console.log(bookingData);
+        $("#meddelande").text(bookingData.message);
+    });
+
+    // Close modal
+    $(document).on('click', '#close-dialog', function () {
+        $('dialog').get(0).close();
+    });
+
     // Returnerar HTML-strängen med alla sektioner och innehåll
     return `
         <div id="kodsidan">
             <div id="kode-main container">
-                <img src="Pages/kodklubb-foto/computer-background.webp" id="Imgtop">
+                <img src="Images/kodklubb-foto/computer-background.webp" id="Imgtop">
                 <div id="kod-landing-container">
                     <div>
                         <h3>Kodklubben</h3>
@@ -105,41 +96,42 @@ export default async function kodklubben() {
                 </div>
             </div>
 
-         <dialog id="kod-dialog">
-          <div>
-             <p id="meddelande">BOKA</p>
-             <form onsubmit="return false">
-                <label for="quantity">Antal:</label>
-                <input type="number" id="quantity" name="quantity" min="1" max="100"></input>
-                <input type="submit" id="kod-submit" value="Boka"></input>
-            </form>
-                     <button id="close-dialog">Close</button>
-                    </div>
-                    </dialog>
+            <dialog id="kod-dialog">
+                <div>
+                    <p id="meddelande">BOKA</p>
+                    <form onsubmit="return false">
+                        <label for="quantity">Antal:</label>
+                        <input type="number" id="quantity" name="quantity" min="1" max="100"></input>
+                        <input type="submit" id="kod-submit" value="Boka"></input>
+                    </form>
+                    <button id="close-dialog">Close</button>
+                </div>
+            </dialog>
 
             <div id="leaderboard-section">
                 <div id="leaderboard-container">
                     <div id="leaderboard-image">
-                    <img src="Pages/kodklubb-foto/leetcode.png" id="leaderboard-image">
+                        <img src="Images/kodklubb-foto/leetcode.png" id="leaderboard-image">
                     </div>
                     <div id="content-boxes">
                         <div class="content-box">
-                        <h2>Rankning för mest leetcode problem gjorda</a></h3>
-                        <ul class=leaderboard-ranks>
-                        <li><span class="rank">1.</span> Linus - 5120</li>
-                        <li><span class="rank">2.</span> Jakob - 4430</li>
-                        <li><span class="rank">3.</span> Kempe - 3230</li>
-                        <li><span class="rank">4.</span> Hoffman - 2054</li>
-                        </ul>
+                            <h2>Rankning för mest leetcode problem gjorda</h3>
+                            <ul class=leaderboard-ranks>
+                                <li><span class="rank">1.</span> Linus - 5120</li>
+                                <li><span class="rank">2.</span> Jakob - 4430</li>
+                                <li><span class="rank">3.</span> Kempe - 3230</li>
+                                <li><span class="rank">4.</span> Hoffman - 2054</li>
+                            </ul>
                         </div>
                         <div class="content-box">
-                        <h2>Snabbaste centrering av div</a></h3>
-                        <ul class=leaderboard-ranks>
-                        <li><span class="rank">1.</span> Jakob - 10 sec</li>
-                        <li><span class="rank">2.</span> Linus - 20 sec</li>
-                        <li><span class="rank">3.</span> Kempe - 40 sec </li>
-                        <li><span class="rank">4.</span> Hoffman - 2 dagar</li>
-                        </ul>
+                            <h2>Snabbaste centrering av div</h3>
+                            <ul class=leaderboard-ranks>
+                                <li><span class="rank">1.</span> Jakob - 10 sec</li>
+                                <li><span class="rank">2.</span> Linus - 20 sec</li>
+                                <li><span class="rank">3.</span> Kempe - 40 sec </li>
+                                <li><span class="rank">4.</span> Hoffman - 2 dagar</li>
+                            </ul>
+                        </div>
                         </div>
                         <div class="content-box">
                         <h2>bästa guidesen gjorda</a></h3>
@@ -163,7 +155,7 @@ export default async function kodklubben() {
                     Vår klubb bildades med syftet att skapa en inkluderande och stödjande miljö där medlemmar kan lära av varandra, dela kunskap och erfarenheter samt utforska den ständigt föränderliga världen av programutveckling.</p><br><br>
                 </div>
                 <div id="about-img-container">
-                    <img src="Pages/kodklubb-foto/kat-programer.png" id="aboutImg">
+                    <img src="Images/kodklubb-foto/kat-programer.png" id="aboutImg">
                 </div>
             </div>
         </div>`;
